f353a6c9b89fa24fcb7b05f3a86fc9a06c0333b7
Mathis Cadio <cadio@et.esiea.fr>
2021-11-30 14:11:15 +0100

    Add test to match 100% coverage

--- src/test/java/fr/lernejo/SampleTest.java
+++ src/test/java/fr/lernejo/SampleTest.java
@@ -7,35 +7,40 @@ class SampleTest {
     @Test
     void op_add_5_and_5_should_produce_10() {
         int a = 5, b = 5;
-        Assertions.assertThat(Sample.op(Sample.Operation.ADD, a, b)).as("add 5 and 5")
+        Sample sample = new Sample();
+        Assertions.assertThat(sample.op(Sample.Operation.ADD, a, b)).as("add 5 and 5")
             .isEqualTo(10);
     }
 
     @Test
     void op_mult_5_and_5_should_produce_25() {
         int a = 5, b = 5;
-        Assertions.assertThat(Sample.op(Sample.Operation.MULT, a, b)).as("mult 5 and 5")
+        Sample sample = new Sample();
+        Assertions.assertThat(sample.op(Sample.Operation.MULT, a, b)).as("mult 5 and 5")
             .isEqualTo(25);
     }
 
     @Test
     void fact_of_negative_should_produce_an_exception() {
         int n = -2;
+        Sample sample = new Sample();
         Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
-            .isThrownBy(() -> Sample.fact(n));
+            .isThrownBy(() -> sample.fact(n));
     }
 
     @Test
     void fact_by_0_should_produce_1() {
         int n = 0;
-        Assertions.assertThat(Sample.fact(n)).as("fact of 0")
+        Sample sample = new Sample();
+        Assertions.assertThat(sample.fact(n)).as("fact of 0")
             .isEqualTo(1);
     }
 
     @Test
     void fact_by_5_should_produce_120() {
         int n = 5;
-        Assertions.assertThat(Sample.fact(n)).as("fact of 5")
+        Sample sample = new Sample();
+        Assertions.assertThat(sample.fact(n)).as("fact of 5")
             .isEqualTo(120);
     }
 }
