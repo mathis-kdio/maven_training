88d182e4349f4751d3ba121789a3cbfdffc727cf
Mathis Cadio <cadio@et.esiea.fr>
2021-11-30 13:40:06 +0100

    Add test to match 100% coverage

---
+++ src/test/java/fr/lernejo/SampleTest.java
@@ -0,0 +1,34 @@
+package fr.lernejo;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SampleTest {
+    @Test
+    void op_add_5_and_5_should_produce_10() {
+        int a = 5, b = 5;
+        Assertions.assertThat(Sample.op(Sample.Operation.ADD, a, b)).as("add 5 and 5")
+            .isEqualTo(10);
+    }
+
+    @Test
+    void op_mult_5_and_5_should_produce_25() {
+        int a = 5, b = 5;
+        Assertions.assertThat(Sample.op(Sample.Operation.MULT, a, b)).as("mult 5 and 5")
+            .isEqualTo(25);
+    }
+
+    @Test
+    void fact_of_negative_should_produce_an_exception() {
+        int n = -2;
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+            .isThrownBy(() -> Sample.fact(n));
+    }
+
+    @Test
+    void fact_by_5_should_produce_120() {
+        int n = 5;
+        Assertions.assertThat(Sample.fact(n)).as("fact of 5")
+            .isEqualTo(120);
+    }
+}
